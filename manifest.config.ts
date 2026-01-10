234299a498c82b0c67d4b3ae3467a071cbeb9a88
kkhys <kkhys@pm.me>
2026-01-10 19:51:46 +0900

    chore(manifest): add icons, description, and title

--- manifest.config.ts
+++ manifest.config.ts
@@ -5,13 +5,22 @@ export default defineManifest({
   manifest_version: 3,
   name,
   version,
+  description: "Copy LGTM image HTML to clipboard",
+  homepage_url: "https://github.com/kkhys/lgtm-chrome-extension",
   icons: {
-    48: "public/logo.png",
+    16: "public/icon-16.png",
+    32: "public/icon-32.png",
+    48: "public/icon-48.png",
+    128: "public/icon-128.png",
   },
   action: {
     default_icon: {
-      48: "public/logo.png",
+      16: "public/icon-16.png",
+      32: "public/icon-32.png",
+      48: "public/icon-48.png",
+      128: "public/icon-128.png",
     },
+    default_title: "Copy LGTM image",
   },
   background: {
     service_worker: "src/background.ts",
